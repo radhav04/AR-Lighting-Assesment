75bf4d4b6664e62e40b2df058891afb148ce7bca
Adhav Rajesh <adhavrajesh04@gmail.com>
2024-04-16 16:17:55 -0400

    Made some changes

--- Assets/SlitController.cs
+++ Assets/SlitController.cs
@@ -12,8 +12,8 @@ public class SlitController : MonoBehaviour
     public GameObject leftBar;
     public Material borderMaterial;
     public TextMeshPro count;
-    private char[] slitOrder = { 'R', 'D', 'U', 'L', 'U', 'R', 'L', 'D', 'U', 'L', 'R', 'D', 'R', 'L', 'D', 'U', 'U', 'L', 'R', 'R', 'D', 'L', 'U', 'D', 'R', 'U', 'D', 'L', 'L', 'R', 'U', 'R', 'D', 'U', 'L', 'D', 'R', 'R', 'U', 'L', 'L', 'R', 'D', 'U', 'D', 'R', 'L', 'U', 'D', 'L', 'R', 'R', 'L', 'U', 'D', 'U', 'D', 'L', 'R', 'R' };
-    private char[] colorOrder = { 'b', 'o', 'g', 'y', 'o', 'r', 'o', 'g', 'r', 'b', 'p', 'y', 'o', 'y', 'p', 'g', 'r', 'b', 'r', 'o', 'b', 'p', 'g', 'y', 'g', 'b', 'o', 'y', 'r', 'p', 'y', 'b', 'r', 'p', 'o', 'g', 'y', 'r', 'p', 'o', 'b', 'g', 'p', 'o', 'y', 'g', 'r', 'b', 'r', 'g', 'o', 'p', 'y', 'b', 'y', 'r', 'b', 'p', 'g', 'o' };
+    private char[] slitOrder = { 'R', 'D', 'L', 'U', 'R', 'U', 'D', 'L', 'R', 'U', 'D', 'L', 'U', 'R', 'D', 'L', 'L', 'U', 'D', 'R', 'U', 'R', 'D', 'L', 'D', 'R', 'L', 'U', 'R', 'D', 'U', 'L', 'R', 'U', 'D', 'L', 'R', 'D', 'L', 'U', 'L', 'U', 'R', 'D', 'R', 'U', 'D', 'L', 'U', 'R', 'L', 'D', 'R', 'U', 'D', 'L', 'R', 'U', 'L', 'D' };
+    private char[] colorOrder = { 'r', 'g', 'o', 'b', 'p', 'y', 'b', 'r', 'g', 'p', 'o', 'y', 'y', 'b', 'g', 'o', 'r', 'p', 'g', 'r', 'b', 'y', 'p', 'o', 'o', 'g', 'b', 'p', 'y', 'r', 'b', 'y', 'p', 'g', 'r', 'o', 'g', 'y', 'r', 'o', 'b', 'p', 'y', 'o', 'g', 'r', 'p', 'b', 'g', 'y', 'p', 'b', 'r', 'o', 'p', 'r', 'g', 'b', 'o', 'y' };
     private int index = -1;
     Color blue = new Color(0, 0, 1);
     Color red = new Color(1, 0, 0);
